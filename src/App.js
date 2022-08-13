e4a586328f282a015200bfd70b22beb02c95404d
leticiatakenaka <leticiatakenaka12@gmail.com>
2022-08-13 13:34:26 -0300

    Mostra dados da API

--- src/App.js
+++ src/App.js
@@ -11,7 +11,24 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => console.log(err));
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const todo = {
@@ -21,12 +38,23 @@ function App() {
       done: false,
     };
 
-    //Envio para API
-    console.log(todo);
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo]);
 
     setTitle("");
     setTime("");
   };
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <div className="App">
@@ -64,6 +92,11 @@ function App() {
       <div className="list-todo">
         <h2>Lista de tarefas:</h2>
         {todos.length === 0 && <p>Não há tarefas!</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <p>{todo.title}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
